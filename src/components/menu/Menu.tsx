111373e83104de00f9d91ec9082cbf7d3908a7af
Galeno de Melo <galenodemelo@gmail.com>
2022-02-27 18:13:07 -0300

    feat: Aligning social links alongside menu links

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,6 +1,7 @@
 import Icon from "@components/icon/Icon"
 import MenuLink from "./menulink/MenuLink"
 import styles from "./Menu.module.sass"
+import { SOCIAL_LINKS } from "src/settings"
 
 export default function Menu(): JSX.Element {
     return (
@@ -13,8 +14,13 @@ export default function Menu(): JSX.Element {
                 <MenuLink href="#contact">Contact</MenuLink>
             </nav>
             <nav className={styles.social}>
-                <Icon filename="instagram.svg" alt="Follow us on Instagram" size="2x" />
-                <Icon filename="facebook.svg" alt="Like us on Facebook" size="2x" />
+                <a href={SOCIAL_LINKS.INSTAGRAM}>
+                    <Icon filename="instagram.svg" alt="Follow us on Instagram" size="2x" />
+                </a>
+
+                <a href={SOCIAL_LINKS.FACEBOOK}>
+                    <Icon filename="facebook.svg" alt="Like us on Facebook" size="2x" />
+                </a>
             </nav>
         </div>
     )
---
+++ src/settings.js
@@ -0,0 +1,9 @@
+const settings = {
+
+    SOCIAL_LINKS: {
+        FACEBOOK: 'https://www.facebook.com/',
+        INSTAGRAM: 'https://www.instagram.com/'
+    }
+}
+
+module.exports = settings
