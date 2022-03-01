497775f4de2952edd003810f5e9129aeb9e6a908
Galeno de Melo <galenodemelo@gmail.com>
2022-03-01 10:12:37 -0300

    feat: Creating PageSwiper component

---
+++ src/components/swiper/page/PageSwiper.tsx
@@ -0,0 +1,27 @@
+import { Swiper, SwiperSlide } from "swiper/react"
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+
+type Props = {
+    className: string,
+    children: React.ReactNode[]
+}
+
+export default function PageSwiper({ className, children }: Props) {
+    return (
+        <Swiper
+            className={className}
+            spaceBetween={0}
+            slidesPerView={1}
+            tag="main"
+        >
+            {children.map((child, index) => {
+                return (
+                    <SwiperSlide tag="section" key={index}>
+                        {child}
+                    </SwiperSlide>
+                )
+            })}
+        </Swiper>
+    )
+}
