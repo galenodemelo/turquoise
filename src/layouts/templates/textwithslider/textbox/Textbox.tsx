4059890d92c645efc56520def2d0b4d6f5d07a6e
Galeno de Melo <galenodemelo@gmail.com>
2022-03-09 11:12:27 -0300

    feat: Requiring a Header and a Paragraph

--- src/layouts/templates/textwithslider/textbox/Textbox.tsx
+++ src/layouts/templates/textwithslider/textbox/Textbox.tsx
@@ -1,3 +1,4 @@
+import Heading from "@components/heading/Heading"
 import styles from "./Textbox.module.sass"
 
 type Props = {
@@ -5,6 +6,10 @@ type Props = {
 }
 
 export default function Textbox({ children }: Props): JSX.Element {
+    const hasHeading: boolean = children.some((child: JSX.Element) => child.type === Heading)
+    const hasParagraph: boolean = children.some((child: JSX.Element) => child.type === "p")
+    if (!hasHeading && !hasParagraph) throw new Error("Textbox must have at least one of Heading or p")
+
     return (
         <div className={styles.textbox}>
             {children}
