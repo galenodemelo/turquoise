a9e99163b2741eeaca6b422df5429fb1cd64f7b3
Galeno de Melo <galenodemelo@gmail.com>
2022-03-09 15:36:08 -0300

    feat: Adding example gallery image

--- src/layouts/home/luxury/Luxury.tsx
+++ src/layouts/home/luxury/Luxury.tsx
@@ -1,7 +1,9 @@
 import Button from "@components/button/Button"
 import Heading from "@components/heading/Heading"
+import GallerySwiper from "@components/swiper/gallery/GallerySwiper"
 import Textbox from "@layouts/templates/textwithslider/textbox/Textbox"
 import TextWithSlider from "@layouts/templates/textwithslider/TextWithSlider"
+import Image from "next/image"
 
 export default function Luxury(): JSX.Element {
     return (
@@ -30,6 +32,18 @@ export default function Luxury(): JSX.Element {
                 </div>
             </Textbox>
 
+            <GallerySwiper>
+                <Image
+                    src="/img/slider/luxury/1-diamond-on-fabric.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                />
+                <Image
+                    src="/img/slider/luxury/1-diamond-on-fabric.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                />
+            </GallerySwiper>
         </TextWithSlider>
     )
 }
