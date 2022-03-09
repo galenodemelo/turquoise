6d3200d8fb14acbbf2a1c35b206d9f14e70decb2
Galeno de Melo <galenodemelo@gmail.com>
2022-03-09 10:23:17 -0300

    feat: Configuring allowed props of component

---
+++ src/components/heading/Heading.tsx
@@ -0,0 +1,16 @@
+import styles from "./Heading.module.sass"
+
+type LineProps = {
+    text: string,
+    color?: "primary" | "secondary",
+    weight?: "light" | "medium"
+}
+
+type Props = {
+    lineList: LineProps[],
+    tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6",
+    size?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+}
+
+export default function Heading({ lineList, tag = "h2", size }: Props): JSX.Element {
+}
