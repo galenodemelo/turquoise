6cf10b8c2a8500158af0c437886bd7853fb6b274
Galeno de Melo <galenodemelo@gmail.com>
2022-03-09 15:20:39 -0300

    feat: Configuring Navigation arrows

--- src/components/swiper/gallery/GallerySwiper.tsx
+++ src/components/swiper/gallery/GallerySwiper.tsx
@@ -1,17 +1,22 @@
+import { Navigation, Swiper as SwiperConfig } from 'swiper'
 import { Swiper, SwiperSlide } from "swiper/react"
 import 'swiper/swiper-bundle.min.css'
 import 'swiper/swiper.min.css'
 import styles from "./GallerySwiper.module.sass"
+
 type Props = {
     children: JSX.Element | JSX.Element[]
 }
 
 export default function GallerySwiper({ children }: Props): JSX.Element {
+    SwiperConfig.use([Navigation])
+
     const slideList = Array.isArray(children) ? children : [children]
 
     return (
         <Swiper
             className={styles.gallerySwiper}
+            navigation={slideList.length > 1}
         >
             {slideList.map((slide, index) => {
                 return (
