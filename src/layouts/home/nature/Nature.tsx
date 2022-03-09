e34775432193af00d8fa5d6e0d65b50386a1fc02
Galeno de Melo <galenodemelo@gmail.com>
2022-03-09 15:53:56 -0300

    feat: Adding example gallery image

--- src/layouts/home/nature/Nature.tsx
+++ src/layouts/home/nature/Nature.tsx
@@ -1,7 +1,9 @@
 import Button from "@components/button/Button"
 import Heading from "@components/heading/Heading"
+import GallerySwiper from "@components/swiper/gallery/GallerySwiper"
 import Textbox from "@layouts/templates/textwithslider/textbox/Textbox"
 import TextWithSlider from "@layouts/templates/textwithslider/TextWithSlider"
+import Image from "next/image"
 
 export default function Nature(): JSX.Element {
     return (
@@ -27,6 +29,19 @@ export default function Nature(): JSX.Element {
                     </Button>
                 </div>
             </Textbox>
+
+            <GallerySwiper>
+                <Image
+                    src="/img/slider/nature/1-couple-walking-on-beach.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                />
+                <Image
+                    src="/img/slider/nature/1-couple-walking-on-beach.jpg"
+                    layout="fill"
+                    objectFit="cover"
+                />
+            </GallerySwiper>
         </TextWithSlider>
     )
 }
