b250806704e883e8a5d4602e70b45aa9f663fb51
Galeno de Melo <galenodemelo@gmail.com>
2022-03-09 15:20:02 -0300

    feat: Applying Swiper for sliding

--- src/components/swiper/gallery/GallerySwiper.tsx
+++ src/components/swiper/gallery/GallerySwiper.tsx
@@ -1,3 +1,7 @@
+import { Swiper, SwiperSlide } from "swiper/react"
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+import styles from "./GallerySwiper.module.sass"
 type Props = {
     children: JSX.Element | JSX.Element[]
 }
@@ -5,4 +9,17 @@ type Props = {
 export default function GallerySwiper({ children }: Props): JSX.Element {
     const slideList = Array.isArray(children) ? children : [children]
 
+    return (
+        <Swiper
+            className={styles.gallerySwiper}
+        >
+            {slideList.map((slide, index) => {
+                return (
+                    <SwiperSlide key={index}>
+                        {slide}
+                    </SwiperSlide>
+                )
+            })}
+        </Swiper>
+    )
 }
