fe0b4f8f32bea99649503640729fa29345fd7fcc
Galeno de Melo <galenodemelo@gmail.com>
2022-03-09 15:25:20 -0300

    fix: Allowing non-HTML elements on Menu

--- src/components/menu/menulink/MenuLink.tsx
+++ src/components/menu/menulink/MenuLink.tsx
@@ -2,7 +2,7 @@ import styles from "./MenuLink.module.sass"
 
 type Props = {
     href: string
-    children: JSX.Element | JSX.Element[]
+    children: string | JSX.Element | JSX.Element[]
 }
 
 export default function MenuLink({ href, children }: Props): JSX.Element {
