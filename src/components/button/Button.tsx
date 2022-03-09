cd4a636b06a78d905d1bf7a841ac08d1d7c92a95
Galeno de Melo <galenodemelo@gmail.com>
2022-03-09 11:52:19 -0300

    feat: Styling button variants

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,3 +1,5 @@
+import styles from "./Button.module.sass"
+
 type Props = {
     children: string | JSX.Element | JSX.Element[]
     href?: string
@@ -6,7 +8,11 @@ type Props = {
 
 export default function Button({ children, href, variant = "primary" }: Props): JSX.Element {
     const Tag = href ? "a" : "button"
+    const buttonClassList = [styles.button, styles[`button--${variant}`]]
 
     return (
+        <Tag href={href} className={buttonClassList.join(" ")}>
+            {children}
+        </Tag>
     )
 }
