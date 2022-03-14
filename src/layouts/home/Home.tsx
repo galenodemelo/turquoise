37640e4f9e8a44886da3f2ad8800a45f3d7517c2
Galeno de Melo <galenodemelo@gmail.com>
2022-03-14 19:09:10 -0300

    feat: Adding `Surrounded` block to Home

--- src/layouts/home/Home.tsx
+++ src/layouts/home/Home.tsx
@@ -1,12 +1,14 @@
 import SliderPage from "@layouts/templates/page/SliderPage"
 import Luxury from "./luxury/Luxury"
 import Nature from "./nature/Nature"
+import Surrounded from "./surround/Surrounded"
 
 export default function Home(): JSX.Element {
     return (
         <SliderPage>
             <Luxury />
             <Nature />
+            <Surrounded />
         </SliderPage>
     )
 }
