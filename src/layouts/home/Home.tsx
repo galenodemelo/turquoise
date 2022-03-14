26afa2ce5685f400e99c48a38283b91e0363833b
Galeno de Melo <galenodemelo@gmail.com>
2022-03-14 19:17:29 -0300

    refactor: Adjusting package name

--- src/layouts/home/Home.tsx
+++ src/layouts/home/Home.tsx
@@ -1,7 +1,7 @@
 import SliderPage from "@layouts/templates/page/SliderPage"
 import Luxury from "./luxury/Luxury"
 import Nature from "./nature/Nature"
-import Surrounded from "./surround/Surrounded"
+import Surrounded from "./surrounded/Surrounded"
 
 export default function Home(): JSX.Element {
     return (
