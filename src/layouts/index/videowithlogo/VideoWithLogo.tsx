b5f31dcabdad52e8d8acc5def117da91aaf14349
Galeno de Melo <galenodemelo@gmail.com>
2022-03-17 22:08:39 -0300

    feat: Full-panel video with autoplay

---
+++ src/layouts/index/videowithlogo/VideoWithLogo.tsx
@@ -0,0 +1,13 @@
+import styles from "./VideoWithLogo.module.sass"
+
+export default function VideoWithLogo(): JSX.Element {
+    return (
+        <div className={styles.videoWithLogo}>
+            <video autoPlay loop muted playsInline className={styles.background}>
+                <source src="/video/beach-surfer-visualizer.webm" type="video/webm" />
+                <source src="/video/beach-surfer-visualizer.mp4" type="video/mp4" />
+            </video>
+
+        </div>
+    )
+}
