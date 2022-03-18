f44a9e056decfee58f830c3722b6d80439a59dec
Galeno de Melo <galenodemelo@gmail.com>
2022-03-17 21:07:01 -0300

    feat: One slider also controls another

--- src/components/swiper/gallery/GallerySwiper.tsx
+++ src/components/swiper/gallery/GallerySwiper.tsx
@@ -1,4 +1,4 @@
-import { Navigation, Swiper as SwiperConfig } from 'swiper'
+import { Controller, Navigation, Swiper as SwiperConfig } from 'swiper'
 import { Swiper, SwiperSlide } from "swiper/react"
 import 'swiper/swiper-bundle.min.css'
 import 'swiper/swiper.min.css'
@@ -6,16 +6,20 @@ import styles from "./GallerySwiper.module.sass"
 
 type Props = {
     children: JSX.Element | JSX.Element[]
+    controller?: any
+    setSwiperInstance?: (swiper: any) => void
 }
 
-export default function GallerySwiper({ children }: Props): JSX.Element {
-    SwiperConfig.use([Navigation])
+export default function GallerySwiper({ children, controller, setSwiperInstance }: Props): JSX.Element {
+    SwiperConfig.use([Controller, Navigation])
 
     const slideList = Array.isArray(children) ? children : [children]
 
     return (
         <Swiper
             className={styles.gallerySwiper}
+            controller={{ control: controller }}
+            onSwiper={setSwiperInstance}
             navigation={slideList.length > 1}
             spaceBetween={0}
             speed={600}
--- src/layouts/index/home/Home.tsx
+++ src/layouts/index/home/Home.tsx
@@ -1,19 +1,24 @@
 import GallerySwiper from "@components/swiper/gallery/GallerySwiper"
 import TextWithSlider from "@layouts/templates/textwithslider/TextWithSlider"
+import { Swiper } from "swiper/react"
+import { useState } from "react"
 import { LuxuryImage, LuxuryTextbox } from "./luxury/Luxury"
 import { NatureImage, NatureTextbox } from "./nature/Nature"
 import { SurroundedImage, SurroundedTextbox } from "./surrounded/Surrounded"
 
 export default function Home(): JSX.Element {
+    const [textboxSwiperInstance, setTextboxSwiperInstance] = useState<Swiper>()
+    const [imageSwiperInstance, setImageSwiperInstance] = useState<Swiper>()
+
     return (
         <TextWithSlider>
-            <GallerySwiper>
+            <GallerySwiper setSwiperInstance={setTextboxSwiperInstance} controller={imageSwiperInstance}>
                 <LuxuryTextbox />
                 <NatureTextbox />
                 <SurroundedTextbox />
             </GallerySwiper>
 
-            <GallerySwiper>
+            <GallerySwiper setSwiperInstance={setImageSwiperInstance} controller={textboxSwiperInstance}>
                 <LuxuryImage />
                 <NatureImage />
                 <SurroundedImage />
