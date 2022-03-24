8d6595f1e0a03097f4b876faa340645cd65d6208
Galeno de Melo <galenodemelo@gmail.com>
2022-03-24 15:20:21 -0300

    feat: Adding photo slider

--- src/layouts/index/areas/gallerywiththumb/GalleryWithThumb.tsx
+++ src/layouts/index/areas/gallerywiththumb/GalleryWithThumb.tsx
@@ -1,3 +1,4 @@
+import GallerySwiper from "@components/swiper/gallery/GallerySwiper"
 import Image from "next/image"
 import styles from "./GalleryWithThumb.module.sass"
 
@@ -22,6 +23,19 @@ export default function GalleryWithThumb({ imageList }: Props): JSX.Element {
                 ))}
             </ul>
 
+            <div className={styles.gallery}>
+                <GallerySwiper>
+                    {imageList.map((image, index) => (
+                        <div key={index} className={styles.galleryItem}>
+                            <Image src={image.url} alt={image.description} layout="fill" objectFit="cover" />
+
+                            <div className={styles.description}>
+                                {image.description}
+                            </div>
+                        </div>
+                    ))}
+                </GallerySwiper>
+            </div>
         </>
     )
 }
