d6320214a2ecdd7a19eb33eb32a6854c82fcc2dd
Galeno de Melo <galenodemelo@gmail.com>
2022-03-24 16:43:45 -0300

    feat: Animating opening the Gallery

--- src/layouts/index/areas/gallerywiththumb/GalleryWithThumb.tsx
+++ src/layouts/index/areas/gallerywiththumb/GalleryWithThumb.tsx
@@ -1,5 +1,6 @@
 import GallerySwiper from "@components/swiper/gallery/GallerySwiper"
 import Image from "next/image"
+import { useState } from "react"
 import styles from "./GalleryWithThumb.module.sass"
 
 export type ThumbProps = {
@@ -13,18 +14,23 @@ type Props = {
 }
 
 export default function GalleryWithThumb({ imageList }: Props): JSX.Element {
+    const [galleryActive, setGalleryActive] = useState<boolean>(false)
+
+    function showGallery(index: number): void {
+        setGalleryActive(true)
+    }
+
     return (
         <>
             <ul className={styles.thumbList}>
                 {imageList.map((image, index) => (
-                    <li key={index} className={styles.item}>
+                    <li key={index} className={styles.item} onClick={() => showGallery(index)}>
                         <Image src={image.thumbUrl} alt={image.description} layout="fill" objectFit="cover" />
                     </li>
                 ))}
             </ul>
 
-            <div className={styles.gallery}>
-                <GallerySwiper>
+            <div className={styles.gallery} data-state-active={galleryActive}>
                     {imageList.map((image, index) => (
                         <div key={index} className={styles.galleryItem}>
                             <Image src={image.url} alt={image.description} layout="fill" objectFit="cover" />
