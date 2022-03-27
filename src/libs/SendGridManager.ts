74caf9bb92372125a1475b160ba284f2d13b97ae
Galeno de Melo <galenodemelo@gmail.com>
2022-03-27 18:45:55 -0300

    refactor: Moving vars to ENV file

--- src/libs/SendGridManager.ts
+++ src/libs/SendGridManager.ts
@@ -2,10 +2,6 @@ import MailService, { MailDataRequired } from "@sendgrid/mail"
 
 class SendGridManager {
 
-    private static DEFAULT_RECEIVER?: string = process.env.CONTACT_FORM_RECEIVER
-    private static DEFAULT_FROM_EMAIL: string = "jeyoti8411@wpsavy.com"
-    private static DEFAULT_FROM_NAME: string = "Turquoise Site"
-
     public static async send(subject: string, body: string, replyTo?: string, cc?: string): Promise<boolean> {
         MailService.setApiKey(process.env.SENDGRID_API_KEY ?? "")
 
@@ -21,12 +17,12 @@ class SendGridManager {
 
     private static buildMessage(subject: string, body: string, replyTo?: string, cc?: string): MailDataRequired {
         return {
-            to: this.DEFAULT_RECEIVER,
+            to: process.env.CONTACT_FORM_RECEIVER,
             cc: cc,
             replyTo: replyTo,
             from: {
-                email: this.DEFAULT_FROM_EMAIL,
-                name: this.DEFAULT_FROM_NAME
+                email: process.env.SENDGRID_FROM_EMAIL ?? "",
+                name: process.env.SENDGRID_FROM_NAME
             },
             subject: subject,
             html: body
