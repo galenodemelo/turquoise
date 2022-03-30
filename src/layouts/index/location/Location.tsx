b58752d53517c7c51c958c85db412f6c2082fc6e
Galeno de Melo <galenodemelo@gmail.com>
2022-03-30 20:40:19 -0300

    refactor: Adjusting map position and moving to a new component

--- src/layouts/index/location/Location.tsx
+++ src/layouts/index/location/Location.tsx
@@ -1,6 +1,7 @@
 import Heading from "@components/heading/Heading"
 import Image from "next/image"
 import styles from "./Location.module.sass"
+import Map from "./map/Map"
 
 export default function Location(): JSX.Element {
     return (
@@ -8,12 +9,7 @@ export default function Location(): JSX.Element {
             <Heading lineList={[{ text: "Local", color: "primary" }]} />
 
             <div className={styles.mapContainer}>
-                <iframe
-                    className={styles.map}
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4869.069786730517!2d-86.23646824768022!3d30.352306525496846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1648425037424!5m2!1spt-BR!2sbr"
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
+                <Map />
 
                 <div className={styles.thumb}>
                     <Image src="/img/map/location-thumb.jpg" layout="fill" objectFit="cover" alt="Overview of Florida location" />
