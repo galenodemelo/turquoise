ee9c292d7fb2ac8fa06072d8dcf6b5e1fbb038d5
Galeno de Melo <galenodemelo@gmail.com>
2022-04-04 11:56:48 -0300

    feat: Making same slide-in animation to every lightbox

--- src/layouts/index/amenities/detailwiththumb/DetailWithThumb.tsx
+++ src/layouts/index/amenities/detailwiththumb/DetailWithThumb.tsx
@@ -3,6 +3,7 @@ import GallerySwiper from "@components/swiper/gallery/GallerySwiper"
 import Textbox from "@layouts/templates/textwithslider/textbox/Textbox"
 import TextWithSlider from "@layouts/templates/textwithslider/TextWithSlider"
 import Image from "next/image"
+import { useState } from "react"
 import styles from "./DetailWithThumb.module.sass"
 
 export type Props = {
@@ -12,19 +13,21 @@ export type Props = {
 }
 
 export default function DetailWithThumb({ title, image, description }: Props): JSX.Element {
+    const [lightboxActive, setLightboxActive] = useState<boolean>(false)
+
     return (
         <li className={styles.item}>
-            <div className={[styles.thumb, "thumbnail"].join(" ")}>
+            <div className={[styles.thumb, "thumbnail"].join(" ")} onClick={() => setLightboxActive(true)}>
                 <Image src={image} alt={title} layout="fill" objectFit="cover" />
             </div>
 
-            <div className={styles.lightbox}>
+            <div className={[styles.lightbox, "slide-in-bottom"].join(" ")} data-state-active={lightboxActive}>
                 <TextWithSlider>
                     <Textbox>
                         <Heading lineList={[{text: title}]} />
                         <p>{description}</p>
 
-                        <button className={styles.back}>Back to amenities</button>
+                        <button className={styles.back} onClick={() => setLightboxActive(false)}>Back to amenities</button>
                     </Textbox>
 
                     <GallerySwiper loop={false}>
--- src/layouts/index/areas/gallerywiththumb/GalleryWithThumb.tsx
+++ src/layouts/index/areas/gallerywiththumb/GalleryWithThumb.tsx
@@ -32,7 +32,7 @@ export default function GalleryWithThumb({ imageList }: Props): JSX.Element {
                 ))}
             </ul>
 
-            <div className={styles.gallery} data-state-active={galleryActive}>
+            <div className={[styles.gallery, "slide-in-bottom"].join(" ")} data-state-active={galleryActive}>
                 <button className={styles.close} onClick={() => setGalleryActive(false)}>
                     <Image src="/img/ico/close.svg" alt="Close gallery" layout="fill" />
                 </button>
