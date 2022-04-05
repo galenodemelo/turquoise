0e4b6738149d72e89b4009ed9f097101bc2218f8
Galeno de Melo <galenodemelo@gmail.com>
2022-04-05 19:11:19 -0300

    fix: Adjusting logo animation to respect order

--- src/layouts/index/videowithlogo/VideoWithLogo.tsx
+++ src/layouts/index/videowithlogo/VideoWithLogo.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image"
+import { useState } from "react"
 import styles from "./VideoWithLogo.module.sass"
 
 export default function VideoWithLogo(): JSX.Element {
+    const [isVideoLoading, setIsVideoLoading] = useState<boolean>(true)
+
     return (
         <div className={styles.videoWithLogo}>
-            <video autoPlay loop muted playsInline className={styles.background}>
+            <video autoPlay loop muted playsInline className={styles.background} preload="true" onPlay={() => setIsVideoLoading(false)}>
                 <source src="/video/beach-surfer-visualizer.webm" type="video/webm" />
                 <source src="/video/beach-surfer-visualizer.mp4" type="video/mp4" />
             </video>
 
-            <div className={styles.logo}>
-                <Image src="/img/turquoise-logo.gif" layout="fill" loading="eager" />
-            </div>
+            {!isVideoLoading &&
+                <div className={styles.logo}>
+                    <Image src="/img/turquoise-logo.gif" layout="fill" loading="eager" alt="Turquoise logo animated" />
+                </div>
+            }
         </div>
     )
 }
