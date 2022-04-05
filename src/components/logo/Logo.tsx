0a07b4c2db50e7f6e8f5990d92d854a8b4783576
Galeno de Melo <galenodemelo@gmail.com>
2022-04-05 19:33:48 -0300

    feat: Adding anchor link to logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,14 +1,15 @@
+import VideoWithLogo from "@layouts/index/videowithlogo/VideoWithLogo"
 import Image from "next/image"
 import styles from "./Logo.module.sass"
 
 export default function Logo(): JSX.Element {
     return (
-        <div className={styles.logo}>
+        <a href={`#${VideoWithLogo.name}`} className={styles.logo}>
             <Image
                 src="/img/turquoise-logo.svg"
                 layout="fill"
                 priority={true}
                 alt="Turquoise green logo" />
-        </div>
+        </a>
     )
 }
