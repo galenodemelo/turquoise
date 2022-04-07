9b183c66ffc588a9f1b343fc935744fd7c534f69
Galeno de Melo <galenodemelo@gmail.com>
2022-04-06 22:11:26 -0300

    feat: Adding attractions section animation

--- src/layouts/index/areas/Areas.tsx
+++ src/layouts/index/areas/Areas.tsx
@@ -1,20 +1,52 @@
 import Heading from "@components/heading/Heading"
+import headingStyles from "@components/heading/Heading.module.sass"
+import { RefObject, useEffect, useRef } from "react"
+import AnimationLib from "src/libs/animation/AnimationLib"
+import AnimationTriggerBuilder from "src/libs/animation/AnimationTriggerBuilder"
 import styles from "./Areas.module.sass"
 import GalleryWithThumb, { ThumbProps } from "./gallerywiththumb/GalleryWithThumb"
+import galleryWithThumbStyle from "./gallerywiththumb/GalleryWithThumb.module.sass"
 
 export default function Areas(): JSX.Element {
+    const slideElement: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null)
+    const imagesList: Array<ThumbProps> = listImages()
+
+    useEffect(() => {
+        let animationList: anime.AnimeInstance[] = []
+        animationList = animationList.concat(
+            AnimationLib.fadeInLetters(slideElement.current?.querySelectorAll(`.${styles.areas} > .${headingStyles.heading} > *`)) ?? [],
+        )
+        for (let i = 1; i <= imagesList.length; i++) {
+            const delay: number = (i - 1) * 200
+            console.log(`.${styles.member}:nth-of-type(${i}) .${headingStyles.heading} > *`)
+            animationList = animationList.concat(
+                AnimationLib.scaleIn(slideElement.current?.querySelectorAll(`.${galleryWithThumbStyle.item}:nth-of-type(${i}) img`), { delay }) ?? [],
+                AnimationLib.fadeIn(slideElement.current?.querySelectorAll(`.${galleryWithThumbStyle.item}:nth-of-type(${i})`), { delay: delay + 600 }) ?? []
+            )
+        }
+
+        new AnimationTriggerBuilder()
+            .setElementToBeObserved(slideElement)
+            .addAnimationFunction(() => {
+                setTimeout(() => {
+                    animationList.forEach((animation: anime.AnimeInstance) => animation.play())
+                }, 800)
+            })
+            .build()
+    }, [slideElement])
+
     return (
-        <div className={styles.areas}>
+        <div className={styles.areas} ref={slideElement}>
             <Heading lineList={[ { text: "Attractions", color: "white" } ]} />
 
             <div className={styles.content}>
-                <GalleryWithThumb imageList={getImageList()} />
+                <GalleryWithThumb imageList={imagesList} />
             </div>
         </div>
     )
 }
 
-function getImageList(): ThumbProps[] {
+function listImages(): Array<ThumbProps> {
     const sliderPath: string = "/img/slider/areas"
 
     return [
