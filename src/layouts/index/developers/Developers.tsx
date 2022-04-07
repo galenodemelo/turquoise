1ae27b57efb5e08c07b1f82c2b6fa33b2f2be062
Galeno de Melo <galenodemelo@gmail.com>
2022-04-07 19:38:15 -0300

    refactor: Adjusting texts

--- src/layouts/index/developers/Developers.tsx
+++ src/layouts/index/developers/Developers.tsx
@@ -61,14 +61,14 @@ function listDevelopers(): Array<DeveloperData>  {
     return [
         {
             photo: "marco.jpg",
-            name: "Marco Vázquez",
-            position: "Member/Owner of a Bravo America",
+            name: "Marco Vazquez",
+            position: "Bravo America Company",
             description: "To Investors: unparalleled risk reward; To The Neighborhood: setting the standard and raising the bar; To Clients: exclusivity, one of a kind standard of living; Wrap it all , this is \"Turquoise\"."
         },
         {
             photo: "luis.jpg",
-            name: "Luís Noronha",
-            position: "Member/Owner of a Lana Development",
+            name: "Luis Noronha",
+            position: "Lana Development",
             description: "The Concept of Turquoise was formed when we first arrived in the 30A.  The great energy and majestic nature of that place were embracing one of the last large parcels available in that area. The uniqueness and exclusivity of our site were something as incredible and precious as a gem."
         }
     ]
