67604058273925e02ae4595dc754796362a8ba13
Galeno de Melo <galenodemelo@gmail.com>
2022-04-06 22:22:59 -0300

    feat: Animating local section

--- src/layouts/index/location/Location.tsx
+++ src/layouts/index/location/Location.tsx
@@ -1,11 +1,34 @@
 import Heading from "@components/heading/Heading"
+import headingStyles from "@components/heading/Heading.module.sass"
 import Image from "next/image"
+import { RefObject, useEffect, useRef } from "react"
+import AnimationLib from "src/libs/animation/AnimationLib"
+import AnimationTriggerBuilder from "src/libs/animation/AnimationTriggerBuilder"
 import styles from "./Location.module.sass"
 import Map from "./map/Map"
 
 export default function Location(): JSX.Element {
+    const slideElement: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        let animationList: anime.AnimeInstance[] = []
+        animationList = animationList.concat(
+            AnimationLib.fadeInLetters(slideElement.current?.querySelectorAll(`.${headingStyles.heading} > *`)) ?? [],
+            AnimationLib.fadeIn(slideElement.current?.querySelectorAll(`.${styles.mapContainer}`), { delay: 800 }) ?? [],
+        )
+
+        new AnimationTriggerBuilder()
+            .setElementToBeObserved(slideElement)
+            .addAnimationFunction(() => {
+                setTimeout(() => {
+                    animationList.forEach((animation: anime.AnimeInstance) => animation.play())
+                }, 800)
+            })
+            .build()
+    }, [slideElement])
+
     return (
-        <div className={styles.location}>
+        <div className={styles.location} ref={slideElement}>
             <Heading lineList={[{ text: "Local", color: "primary" }]} />
 
             <div className={styles.mapContainer}>
