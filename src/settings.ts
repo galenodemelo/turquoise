b6c42c71330e6d64b5ff05db2d8474430137f33f
Galeno de Melo <galenodemelo@gmail.com>
2022-06-06 20:15:52 -0300

    chore: Updating phone number

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,6 @@
 const SETTINGS = {
     GOOGLE_RECAPTCHA_URL: 'https://www.google.com/recaptcha/api/siteverify',
-    PHONE_NUMBER: "+1 (850) 733-7574",
+    PHONE_NUMBER: "+1 (850) 738-1551",
     SOCIAL_LINKS: {
         FACEBOOK: 'https://www.facebook.com/turquoise.fl/',
         INSTAGRAM: 'https://www.instagram.com/turquoise.homes/'
