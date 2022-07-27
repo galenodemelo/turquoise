fbbba2dfc68106e2d62e4f9b0172dcd6c10fa18a
Galeno de Melo <galenodemelo@gmail.com>
2022-07-27 18:12:44 -0300

    feat: Embedding video

---
+++ pages/presentation-video.tsx
@@ -0,0 +1,14 @@
+export default () => {
+    return (
+        <iframe
+            src="https://player.vimeo.com/video/733044311?h=ed0ebae5dc"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+            style={{
+                position: "absolute",
+                height: "100%",
+                width: "100%"
+            }}
+        ></iframe>
+    )
+}
