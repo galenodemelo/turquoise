c96dd3923c21abef5357923f6018c22ebbd511f6
Galeno de Melo <galenodemelo@gmail.com>
2023-01-11 20:08:14 -0300

    feat: Creating full-size video component

---
+++ src/components/Backdrop/BackdropVideo.tsx
@@ -0,0 +1,7 @@
+import { Section } from "@layouts/VerticalSwipePage/Section"
+import styled from "styled-components"
+
+export const BackdropVideo = styled.video`
+    ${Section}
+    object-fit: cover;
+`
---
+++ src/layouts/VerticalSwipePage/Section/index.tsx
@@ -0,0 +1,10 @@
+import { css } from "styled-components";
+
+export const Section = css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    min-height: 100vh;
+    max-height: 100%;
+`
