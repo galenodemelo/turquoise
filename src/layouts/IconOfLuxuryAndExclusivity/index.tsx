0e5bea15d1e8d9433dce7837185bcb04e78ee1fd
Galeno de Melo <galenodemelo@gmail.com>
2023-01-16 11:16:36 -0300

    feat: Adding content to the box

--- src/layouts/IconOfLuxuryAndExclusivity/index.tsx
+++ src/layouts/IconOfLuxuryAndExclusivity/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import React from "react";
 import { Bar, Icon, IconOfLuxuryAndExclusivityWrapper, Text } from "./style";
 
 export default class IconOfLuxuryAndExclusivity extends React.Component {
@@ -5,6 +7,23 @@ export default class IconOfLuxuryAndExclusivity extends React.Component {
     render(): JSX.Element {
         return (
             <IconOfLuxuryAndExclusivityWrapper>
+                <Bar />
+
+                <Text>
+                    <h2>Icon of luxury and exclusivity</h2>
+                    <p>
+                        Toss your troubles away as you float on the gulf <br />
+                        under the glorious blue sky and over <br />
+                        translucent waters. Get away from it all, set <br />
+                        anchor, and while away the hours with only your <br />
+                        loved ones by your side. Truly heaven <br />
+                        on Earth.
+                    </p>
+                </Text>
+
+                <Icon>
+                    <Image src="/img/turquoise-picto.svg" width={124} height={124} alt="Turquoise Q pictogram" />
+                </Icon>
             </IconOfLuxuryAndExclusivityWrapper>
         )
     }
--- src/layouts/IconOfLuxuryAndExclusivity/style.ts
+++ src/layouts/IconOfLuxuryAndExclusivity/style.ts
@@ -10,3 +10,35 @@ export const IconOfLuxuryAndExclusivityWrapper = styled.div`
     align-items: center;
     background-color: ${COLORS.green};
 `;
+
+export const Bar = styled.div`
+    flex: 2;
+    width: 3px;
+    background-color: #ffffff;
+`;
+
+export const Text = styled.div`
+    flex: 1;
+    display: inline-flex;
+    flex-direction: column;
+    justify-content: center;
+    row-gap: 56px;
+    color: #ffffff;
+    text-align:center;
+
+    h2 {
+        font-size: 1.8rem;
+        font-weight: 500;
+    }
+
+    p {
+        font-size: 1.2rem;
+        font-weight: 300;
+        line-height: 1.8;
+    }
+`;
+
+export const Icon = styled.div`
+    position: relative;
+    flex: 1;
+`;
