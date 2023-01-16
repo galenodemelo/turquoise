2b16bfe2a0553d451ab3a0dd65d78fbec598057c
Galeno de Melo <galenodemelo@gmail.com>
2023-01-16 09:20:54 -0300

    feat: Adding aerial view link

--- src/layouts/FloatingMenu/Extra/index.tsx
+++ src/layouts/FloatingMenu/Extra/index.tsx
@@ -7,6 +7,10 @@ export default class Extra extends React.Component {
     render(): JSX.Element {
         return (
             <ExtraWrapper>
+                <AerialView href="#">
+                    Aerial View
+                </AerialView>
+
                 <SocialLinkWrapper>
                     <SocialLink href="#">
                         <Image src="/img/ico/instagram-logo.svg" alt="Instagram logo" title="Follow us on Instagram" width={24} height={24} />
--- src/layouts/FloatingMenu/Extra/style.ts
+++ src/layouts/FloatingMenu/Extra/style.ts
@@ -7,6 +7,26 @@ export const ExtraWrapper = styled.div`
     column-gap: 40px;
 `;
 
+export const AerialView = styled.a`
+    display: inline-flex;
+    align-items: center;
+    column-gap: 8px;
+    color: #ffffff;
+    line-height: 1;
+    font-size: 1.2rem;
+
+    &::after {
+        position: relative;
+        top: -2px;
+        display: inline-block;
+        content: "";
+        width: 20px;
+        height: 20px;
+        background: url("/img/ico/arrow-circled.svg") center center no-repeat transparent;
+        background-size: contain;
+    }
+`;
+
 export const SocialLinkWrapper = styled.div`
     display: inline-flex;
     align-items: center;
