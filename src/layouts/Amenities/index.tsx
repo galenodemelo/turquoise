afcb765f7bfb149f95bc0c318d97746692aacf94
Galeno de Melo <galenodemelo@gmail.com>
2023-01-17 21:49:43 -0300

    feat: Adding accordion data

--- src/layouts/Amenities/index.tsx
+++ src/layouts/Amenities/index.tsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { Accordion, AccordionContent, AccordionHeader, AccordionList, AmenitiesWrapper, Carousel, Details, DetailsFooter, DetailsHeading, ImageItem, ImageWrapper } from "./style";
+
 interface State {
     activeIndex: number;
 }
 
+export interface ImageItemWraperProps {
+    visible: boolean
+}
+
 export default class Amenities extends React.Component<{}, State> {
+
+    accordionList: Array<{ header: string, content: string, imagePath: string }> = [
+        {
+            header: "Entrance",
+            content: "Wadding pool, swimming pool, jacuzzi, sun deck, lanai, community room, dry sauna and cold shower, sauna relax deck, bathrooms, workout facility, playroom, exterior yoga terrace, gourmet kitchen and party room.",
+            imagePath: "/img/carousel/amenities/clubhouse.jpg"
+        },
+        {
+            header: "Club House",
+            content: "Wadding pool, swimming pool, jacuzzi, sun deck, lanai, community room, dry sauna and cold shower, sauna relax deck, bathrooms, workout facility, playroom, exterior yoga terrace, gourmet kitchen and party room.",
+            imagePath: "/img/carousel/amenities/clubhouse.jpg"
+        },
+        {
+            header: "Club House Pool",
+            content: "Wadding pool, swimming pool, jacuzzi, sun deck, lanai, community room, dry sauna and cold shower, sauna relax deck, bathrooms, workout facility, playroom, exterior yoga terrace, gourmet kitchen and party room.",
+            imagePath: "/img/carousel/amenities/clubhouse-pool.jpg"
+        },
+        {
+            header: "5-Story Observation Tower",
+            content: "Wadding pool, swimming pool, jacuzzi, sun deck, lanai, community room, dry sauna and cold shower, sauna relax deck, bathrooms, workout facility, playroom, exterior yoga terrace, gourmet kitchen and party room.",
+            imagePath: "/img/carousel/amenities/5-story-observation-tower.jpg"
+        },
+        {
+            header: "+ 3.000 ft. of trails",
+            content: "Wadding pool, swimming pool, jacuzzi, sun deck, lanai, community room, dry sauna and cold shower, sauna relax deck, bathrooms, workout facility, playroom, exterior yoga terrace, gourmet kitchen and party room.",
+            imagePath: "/img/carousel/amenities/clubhouse.jpg"
+        },
+        {
+            header: "Fire pits",
+            content: "Wadding pool, swimming pool, jacuzzi, sun deck, lanai, community room, dry sauna and cold shower, sauna relax deck, bathrooms, workout facility, playroom, exterior yoga terrace, gourmet kitchen and party room.",
+            imagePath: "/img/carousel/amenities/fire-pits.jpg"
+        },
+        {
+            header: "Pickeball court",
+            content: "Wadding pool, swimming pool, jacuzzi, sun deck, lanai, community room, dry sauna and cold shower, sauna relax deck, bathrooms, workout facility, playroom, exterior yoga terrace, gourmet kitchen and party room.",
+            imagePath: "/img/carousel/amenities/pickeball-court.jpg"
+        },
+    ]
 
     constructor(props: {}) {
         super(props)
@@ -18,9 +62,31 @@ export default class Amenities extends React.Component<{}, State> {
                 <Carousel>
                     <Details>
                         <DetailsHeading>Amenities</DetailsHeading>
+
+                        <AccordionList>
+                            {this.accordionList.map((item, index) => {
+                                return (
+                                    <Accordion key={index} open={this.state.activeIndex === index}>
+                                        <AccordionHeader onClick={(event) => {
+                                            event.preventDefault()
+                                        }}>
+                                            {item.header}
+                                        </AccordionHeader>
+                                        <AccordionContent>
+                                            {item.content}
+                                        </AccordionContent>
+                                    </Accordion>
+                                )
+                            })}
+                        </AccordionList>
                     </Details>
 
                     <ImageWrapper>
+                        {this.accordionList.map((item, index) => {
+                            return (
+                                <ImageItem src={item.imagePath} alt={item.header} visible={this.state.activeIndex === index} key={index} />
+                            )
+                        })}
                     </ImageWrapper>
                 </Carousel>
 
--- src/layouts/Amenities/style.ts
+++ src/layouts/Amenities/style.ts
@@ -3,7 +3,10 @@ import {
     sectionHeadingExtraMargin,
     SectionPadded,
 } from "@layouts/VerticalSwipePage/Section";
+import { COLORS } from "@styles/globals";
 import styled from "styled-components";
+import { ImageItemWraperProps } from ".";
+
 export const AmenitiesWrapper = styled.div`
     ${SectionPadded}
     padding-right: 0;
@@ -32,8 +35,48 @@ export const DetailsHeading = styled(SectionHeading)`
     margin-left: 0;
 `;
 
+export const AccordionList = styled.div`
+    display: inline-flex;
+    flex-direction: column;
+    row-gap: 32px;
+`;
+
+export const Accordion = styled.details`
+    > summary {
+        list-style: none;
+    }
+
+    > summary::-webkit-details-marker {
+        display: none;
+    }
+`;
+
+export const AccordionHeader = styled.summary`
+    color: ${COLORS.darkGreen};
+    font-size: 1.2rem;
+    cursor: pointer;
+    font-weight: 500;
+`;
+
+export const AccordionContent = styled.p`
+    margin-top: 16px;
+    color: ${COLORS.darkGreen};
+    font-size: 0.8rem;
+    line-height: 1.6;
+`;
+
 export const ImageWrapper = styled.div`
     position: relative;
     width: 100%;
     align-self: stretch;
+`;
+
+export const ImageItem = styled.img<ImageItemWraperProps>`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: ${(props) => (props.visible ? 1 : 0)};
+    transition: opacity 0.6s ease;
 `;
