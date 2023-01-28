bd3fd41977d58549c0bcfb857cffab63506e9516
Galeno de Melo <galenodemelo@gmail.com>
2023-01-28 18:40:26 -0300

    refactor: Standard code style

--- src/layouts/VerticalSwipePage/Section/index.ts
+++ src/layouts/VerticalSwipePage/Section/index.ts
@@ -9,20 +9,20 @@ export const Section = css`
     width: 100%;
     min-height: 100vh;
     max-height: 100%;
-`
+`;
 
 export const SectionPadded = css`
     ${Section}
     padding: 8% ${horizontalMargin};
     box-sizing: border-box;
-`
+`;
 
-export const sectionHeadingExtraMargin = "96px"
+export const sectionHeadingExtraMargin = "96px";
 
 export const SectionHeading = styled.div`
-	font-size: 1.6rem;
+    font-size: 1.6rem;
     font-weight: bold;
     line-height: 1;
     color: ${COLORS.darkGreen};
     margin-left: ${sectionHeadingExtraMargin};
-`
+`;
