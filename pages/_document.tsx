54ebb9a816c36a406c91d3d61755dfb27b47b700
Galeno de Melo <galenodemelo@gmail.com>
2023-01-30 18:23:10 -0300

    fix: Avoiding CSS flickering

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps> {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: [
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ]
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
+}
+
+// Source: https://stackoverflow.com/questions/67087999/how-to-properly-type-the-document-tsx-file-from-next-js
