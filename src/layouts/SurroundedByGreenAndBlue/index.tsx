de856900a7d51c8386bcccb87de859f1b032d08d
Galeno de Melo <galenodemelo@gmail.com>
2023-01-30 11:11:06 -0300

    fix: Avoiding circular reference

--- src/layouts/SurroundedByGreenAndBlue/index.tsx
+++ src/layouts/SurroundedByGreenAndBlue/index.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import React from "react";
+import { SurroundedByGreenAndBlueWrapper } from "./style";
 
 export default class SurroundedByGreenAndBlue extends React.Component {
 
     render(): JSX.Element {
         return (
-            <SurroundedByGreenAndBlue>
+            <SurroundedByGreenAndBlueWrapper>
                 <Image layout="fill" objectFit="cover" objectPosition="center center" src="/img/carousel/concept/surrounded-by-green-and-blue.jpg" alt="Father, mother and daughter playing golf at the sundown and the title 'Surrounded by green and blue'" />
-            </SurroundedByGreenAndBlue>
+            </SurroundedByGreenAndBlueWrapper>
         )
     }
 }
