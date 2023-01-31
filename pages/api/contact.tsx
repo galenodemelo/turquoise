52881efdf9bc1a64cfaeb91ed9304cc367efbba5
Galeno de Melo <galenodemelo@gmail.com>
2023-01-31 15:29:35 -0300

    feat: Sending mail via SendGrid

--- pages/api/contact.tsx
+++ pages/api/contact.tsx
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import ApiResponse from "src/libs/ApiResponse";
 import ContactFormModel from "src/libs/database/ContactFormModel";
+import SendGridManager from "src/libs/mail/SendGridManager";
 
 export interface ContactPayload {
     name: string
@@ -20,10 +21,49 @@ export default async function ApiContact(req: NextApiRequest, res: NextApiRespon
         }
 
         const payload: ContactPayload = req.body
-        await ContactFormModel.save(payload)
+
+        let promiseList: Promise<any>[] = []
+
+        const storeInDatabasePromise: Promise<void> = ContactFormModel.save(payload)
+        promiseList.push(storeInDatabasePromise)
+
+        let mailWasSent = false
+        const mailSendPromise: Promise<boolean> = SendGridManager.send(
+            `Site contact form - ${payload.name} ${payload.lastName}`,
+            buildMailHtml(payload),
+            payload.email,
+            payload.email
+        ).then((success: boolean) => mailWasSent = success)
+        promiseList.push(mailSendPromise)
+
+        await Promise.all(promiseList)
+        if (!mailWasSent) {
+            res.status(500).json(ApiResponse.error("Mail was not sent. Please, try again."))
+            return
+        }
+
         res.status(200).json(ApiResponse.success("Contact sent successfully!"))
     } catch (error) {
         console.error("ApiContact >> Unknown error", `Data: [${JSON.stringify(req.body)}]`, error);
         res.status(500).json(ApiResponse.error("Unknown error. Please, contact support"))
     }
+}
+
+function buildMailHtml({ name, lastName, email, phone, origin, specify, message }: ContactPayload): string {
+    let mailHtml: string = `<h1>Contact form</h1>`
+
+    let fieldList: Array<string> = []
+    fieldList.push(`<strong>Name:</strong> ${name}`)
+    fieldList.push(`<strong>Last name:</strong> ${lastName}`)
+    fieldList.push(`<strong>E-mail:</strong> ${email}`)
+    fieldList.push(`<strong>Phone:</strong> ${phone}`)
+
+    if (origin) fieldList.push(`<strong>How did you meet us:</strong> ${origin}`)
+    if (specify) fieldList.push(`<strong>Please, specify:</strong> ${specify}`)
+
+    fieldList.push(`<strong>Message:</strong> <br />${message.split("\n").join("<br />")}`)
+
+    mailHtml += `<p>${fieldList.join("<br />")}</p>`
+
+    return mailHtml
 }
---
+++ src/libs/mail/SendGridManager.ts
@@ -0,0 +1,44 @@
+import MailService, { MailDataRequired } from "@sendgrid/mail";
+
+class SendGridManager {
+    public static async send(
+        subject: string,
+        body: string,
+        replyTo?: string,
+        cc?: string
+    ): Promise<boolean> {
+        MailService.setApiKey(process.env.SENDGRID_API_KEY ?? "");
+
+        const message: MailDataRequired = this.buildMessage(subject, body, replyTo, cc);
+
+        return await MailService.sendMultiple(message)
+            .then(() => {
+                return true;
+            })
+            .catch((error) => {
+                console.error("SendGridManager.send >>", JSON.stringify(error), error);
+                return false;
+            });
+    }
+
+    private static buildMessage(
+        subject: string,
+        body: string,
+        replyTo?: string,
+        cc?: string
+    ): MailDataRequired {
+        return {
+            to: process.env.CONTACT_FORM_RECEIVER,
+            cc: cc,
+            replyTo: replyTo,
+            from: {
+                email: process.env.SENDGRID_FROM_EMAIL ?? "",
+                name: "Turquoise Site",
+            },
+            subject: subject,
+            html: body,
+        };
+    }
+}
+
+export default SendGridManager;
