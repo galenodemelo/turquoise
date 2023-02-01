a26eb4bf3402279c9707ef81c3955061a57cddcc
Galeno de Melo <galenodemelo@gmail.com>
2023-02-01 11:45:42 -0300

    feat: Animating block

--- src/layouts/IconOfLuxuryAndExclusivity/style.ts
+++ src/layouts/IconOfLuxuryAndExclusivity/style.ts
@@ -1,19 +1,12 @@
 import { AnimateOnActiveComponentProps } from "@layouts/AnimateOnActiveComponent";
 import { Section } from "@layouts/VerticalSwipePage/Section";
+import { dissolveDefaultAnimation } from "@styles/animations";
 import { COLORS } from "@styles/globals";
 import styled from "styled-components";
 
-export const IconOfLuxuryAndExclusivityWrapper = styled.div`
-    ${Section}
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
-    background-color: ${COLORS.green};
-`;
-
 export const Bar = styled.div`
-    flex: 2;
+    transition: flex 3s ease;
+    flex: 0;
     width: 3px;
     background-color: #ffffff;
 `;
@@ -25,7 +18,10 @@ export const Text = styled.div`
     justify-content: center;
     row-gap: 56px;
     color: #ffffff;
-    text-align:center;
+    text-align: center;
+
+    ${dissolveDefaultAnimation};
+    animation-delay: 3s;
 
     h2 {
         font-size: 1.8rem;
@@ -42,4 +38,31 @@ export const Text = styled.div`
 export const Icon = styled.div`
     position: relative;
     flex: 1;
+
+    ${dissolveDefaultAnimation};
+    animation-delay: 3.5s;
+`;
+
+export const IconOfLuxuryAndExclusivityWrapper = styled.div<AnimateOnActiveComponentProps>`
+    ${Section}
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: ${COLORS.green};
+
+    ${(props) => {
+        if (!props.triggeredAnimation) return ``;
+
+        return `
+            ${Bar} {
+                flex: 2;
+            }
+
+            ${Text},
+            ${Icon} {
+                animation-play-state: running;
+            }
+        `;
+    }}
 `;
--- src/styles/animations.ts
+++ src/styles/animations.ts
@@ -1,6 +1,20 @@
-import { keyframes } from "styled-components";
+import { css, keyframes } from "styled-components";
 
 export const horizontalGrowShrink = keyframes`
     0% { width: 60%; }
-    100% { width: 100% }
-`
+    100% { width: 100%; }
+`;
+
+export const dissolve = keyframes`
+    0% { opacity: 0; }
+    100% { opacity: 1; }
+`;
+
+export const dissolveDefaultAnimation = css`
+    opacity: 0;
+    animation-name: ${dissolve};
+    animation-duration: 1.5s;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+    animation-play-state: paused;
+`;
