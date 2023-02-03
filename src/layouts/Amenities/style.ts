72a564f055883e61d59d55fcca5842cb1d7c93e8
Galeno de Melo <galenodemelo@gmail.com>
2023-02-03 15:29:33 -0300

    refactor: Adapting Amenities to mobile

--- src/layouts/Amenities/style.ts
+++ src/layouts/Amenities/style.ts
@@ -1,9 +1,10 @@
+import { horizontalMobileMargin } from "@layouts/FloatingMenu/style";
 import {
     SectionHeading,
     sectionHeadingExtraMargin,
     SectionPadded,
 } from "@layouts/VerticalSwipePage/Section";
-import { COLORS } from "@styles/globals";
+import { BREAKPOINTS, COLORS } from "@styles/globals";
 import styled from "styled-components";
 import { ImageItemWraperProps } from ".";
 
@@ -21,6 +22,11 @@ export const Carousel = styled.div`
     width: 100%;
     padding-left: ${sectionHeadingExtraMargin};
     column-gap: 40px;
+
+    ${BREAKPOINTS.upToTablet} {
+        padding: 0 ${horizontalMobileMargin} 32px;
+        flex-direction: column;
+    }
 `;
 
 export const Details = styled.div`
@@ -29,6 +35,11 @@ export const Details = styled.div`
     justify-content: space-between;
     padding-bottom: ${sectionHeadingExtraMargin};
     flex: 0 0 500px;
+
+    ${BREAKPOINTS.upToTablet} {
+        flex: 0 0 0;
+        padding-bottom: 24px;
+    }
 `;
 
 export const DetailsHeading = styled(SectionHeading)`
@@ -39,6 +50,14 @@ export const AccordionList = styled.div`
     display: inline-flex;
     flex-direction: column;
     row-gap: 32px;
+    padding-top: 32px;
+
+    ${BREAKPOINTS.upToTablet} {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: auto;
+        row-gap: 16px;
+    }
 `;
 
 export const Accordion = styled.details`
@@ -58,6 +77,13 @@ export const AccordionHeader = styled.summary`
     font-weight: 500;
     transition: color 0.2s ease;
 
+    ${BREAKPOINTS.upToTablet} {
+        font-size: 0.7rem;
+        > br {
+            display: none;
+        }
+    }
+
     &:hover,
     &[data-state-active="true"] {
         color: ${COLORS.darkGreen};
@@ -69,6 +95,10 @@ export const AccordionContent = styled.p`
     color: ${COLORS.green};
     font-size: 0.8rem;
     line-height: 1.6;
+
+    ${BREAKPOINTS.upToTablet} {
+        display: none;
+    }
 `;
 
 export const DetailsFooter = styled.div`
@@ -76,12 +106,23 @@ export const DetailsFooter = styled.div`
     justify-content: space-between;
     color: ${COLORS.darkGreen};
     font-size: 0.8rem;
+
+    ${BREAKPOINTS.upToTablet} {
+        position: absolute;
+        bottom: 16px;
+        font-size: 0.7rem;
+    }
 `;
 
 export const ImageWrapper = styled.div`
     position: relative;
     width: 100%;
     align-self: stretch;
+
+    ${BREAKPOINTS.upToTablet} {
+        height: 100%;
+        flex: 1 1 0;
+    }
 `;
 
 export const ImageItem = styled.img<ImageItemWraperProps>`
