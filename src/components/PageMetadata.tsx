8955682ebe2ef6616c0fb4d128d44a5fa5894e66
Galeno de Melo <galenodemelo@gmail.com>
2023-02-07 09:40:10 -0300

    feat: Adding favicon

--- src/components/PageMetadata.tsx
+++ src/components/PageMetadata.tsx
@@ -6,6 +6,7 @@ export default class PageMetadata extends React.Component {
     render(): JSX.Element {
         return (
             <Head>
+                <link rel="shortcut icon" href="/img/favicon.png" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Turquoise &middot; An icon of luxury and exclusivity</title>
             </Head>
