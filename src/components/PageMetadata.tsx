6d2c806f0b5a6d4ef00682421b98d74ddd9ba53c
Galeno de Melo <galenodemelo@gmail.com>
2023-02-07 09:41:13 -0300

    feat: Adding thumbnail

--- src/components/PageMetadata.tsx
+++ src/components/PageMetadata.tsx
@@ -9,6 +9,8 @@ export default class PageMetadata extends React.Component {
                 <link rel="shortcut icon" href="/img/favicon.png" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Turquoise &middot; An icon of luxury and exclusivity</title>
+
+                <meta property="og:image" content="/img/social-media-thumbnail.jpg" />
             </Head>
         )
     }
