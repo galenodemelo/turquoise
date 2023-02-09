d71456acb32bfd9c7a2d91d620fc751e724aaa4d
Galeno de Melo <galenodemelo@gmail.com>
2023-02-09 10:57:33 -0300

    refactor: Dividing menu from content on mobile

--- src/layouts/FloatingMenu/style.ts
+++ src/layouts/FloatingMenu/style.ts
@@ -1,4 +1,4 @@
-import { BREAKPOINTS, SETTINGS } from "@styles/globals";
+import { BREAKPOINTS, COLORS, SETTINGS } from "@styles/globals";
 import styled from "styled-components";
 import { FloatingMenuProps } from ".";
 import { ExtraWrapper } from "./Extra/style";
@@ -34,10 +34,19 @@ export const Header = styled.header<FloatingMenuProps>`
     z-index: 1000;
     box-sizing: border-box;
     opacity: ${(props) => (props.visible ? 1 : 0)};
-    transition: opacity ${SETTINGS.swiper.speedInSeconds}s ease;
+    transition: all ${SETTINGS.swiper.speedInSeconds}s ease;
 
     ${BREAKPOINTS.upToTablet} {
         padding: 12px 16px;
+
+        ${(props) => {
+            if (props.white) return "";
+
+            return `
+            background-color: #ffffff;
+            border-bottom: ${COLORS.green} solid 1px;
+        `;
+        }}
     }
 
     > * {
