418434b42cf7b68b34415e92ee9d6650d89109c1
Galeno de Melo <galenodemelo@gmail.com>
2023-03-08 18:10:34 -0300

    refactor: Making it a constant

--- src/layouts/Attraction/index.tsx
+++ src/layouts/Attraction/index.tsx
@@ -3,30 +3,37 @@ import React from "react";
 import { AttractionWrapper, ImageCaption, LocationGalleryItem } from "./style";
 
 interface State {
-    expandedItemIndex: number | null
+    expandedItemIndex: number | null;
+}
+
+interface AttractionItem {
+    caption: string;
+    imagePath: string;
+    alt: string;
+    imagePosition: string;
 }
 
 export default class Attraction extends React.Component<{}, State> {
 
+    static readonly attractionItemList: AttractionItem[] = [
+        { caption: "Dunes", imagePath: "/img/carousel/attraction/dunes.jpg", alt: "View of dunes with a little wood bridge", imagePosition: "center center" },
+        { caption: "Santa Rosa Beach", imagePath: "/img/carousel/attraction/santa-rosa-beach.jpg", alt: "Aerial view of Santa Rosa Beach", imagePosition: "left center" },
+        { caption: "Highway 30A", imagePath: "/img/carousel/attraction/highway-30a.jpg", alt: "Aerial view of Highway 30A", imagePosition: "center center" },
+        { caption: "Golf Club", imagePath: "/img/carousel/attraction/golf-club.jpg", alt: "Aerial view of golf club with some trees aside", imagePosition: "right center" },
+        { caption: "Fox Lake", imagePath: "/img/carousel/attraction/fox-lake.jpg", alt: "Aerial view of fox lake", imagePosition: "center center" }
+    ];
+
     constructor(props: {}) {
-        super(props)
+        super(props);
         this.state = {
             expandedItemIndex: null
-        }
+        };
     }
 
     render(): JSX.Element {
-        const itemList: Array<{ caption: string, imagePath: string, alt: string, imageMobileObjectPosition: string }> = [
-            { caption: "Dunes", imagePath: "/img/carousel/attraction/dunes.jpg", alt: "View of dunes with a little wood bridge", imageMobileObjectPosition: "center center" },
-            { caption: "Santa Rosa Beach", imagePath: "/img/carousel/attraction/santa-rosa-beach.jpg", alt: "Aerial view of Santa Rosa Beach", imageMobileObjectPosition: "left center" },
-            { caption: "Highway 30A", imagePath: "/img/carousel/attraction/highway-30a.jpg", alt: "Aerial view of Highway 30A", imageMobileObjectPosition: "center center" },
-            { caption: "Golf Club", imagePath: "/img/carousel/attraction/golf-club.jpg", alt: "Aerial view of golf club with some trees aside", imageMobileObjectPosition: "right center" },
-            { caption: "Fox Lake", imagePath: "/img/carousel/attraction/fox-lake.jpg", alt: "Aerial view of fox lake", imageMobileObjectPosition: "center center" }
-        ]
-
         return (
             <AttractionWrapper>
-                {itemList.map((item, index) => {
+                {Attraction.attractionItemList.map((item, index) => {
                     return (
                         <LocationGalleryItem
                             key={index}
