8e1f8621558445ae6b94826ea71565a8c52cd1ae
Galeno de Melo <galenodemelo@gmail.com>
2023-03-14 09:25:29 -0300

    fix: Avoiding indexing issues

--- pages/presentation-video.tsx
+++ pages/presentation-video.tsx
@@ -1,14 +1,25 @@
+import PageMetadata from "@components/PageMetadata";
+import Head from "next/head";
+
 export default function PresentationVideo() {
     return (
-        <iframe
-            src="https://player.vimeo.com/video/733044311?h=ed0ebae5dc"
-            allow="autoplay; fullscreen; picture-in-picture"
-            allowFullScreen
-            style={{
-                position: "absolute",
-                height: "100%",
-                width: "100%"
-            }}
-        ></iframe>
-    )
+        <>
+            <Head>
+                <meta name="robots" content="noindex" />
+            </Head>
+
+            <PageMetadata />
+
+            <iframe
+                src="https://player.vimeo.com/video/733044311?h=ed0ebae5dc"
+                allow="autoplay; fullscreen; picture-in-picture"
+                allowFullScreen
+                style={{
+                    position: "absolute",
+                    height: "100%",
+                    width: "100%"
+                }}
+            ></iframe>
+        </>
+    );
 }
