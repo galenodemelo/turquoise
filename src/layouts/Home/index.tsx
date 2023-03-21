70491564743fe016400d35732b9aeb7341194b93
Galeno de Melo <galenodemelo@gmail.com>
2023-03-21 15:49:36 -0300

    refactor: Consuming less bandwidth

--- src/layouts/Home/index.tsx
+++ src/layouts/Home/index.tsx
@@ -2,15 +2,35 @@ import { BackdropVideo } from "@components/Backdrop/BackdropVideo";
 import React from "react";
 import { HomeWrapper } from "./style";
 
-export default class Home extends React.Component {
+interface State {
+    isBigScreen?: boolean;
+}
+
+export default class Home extends React.Component<{}, State> {
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount(): void {
+        const isBigScreen = window.innerWidth > 1660;
+        this.setState({ isBigScreen: isBigScreen });
+    }
 
     render(): JSX.Element {
         return (
             <HomeWrapper>
                 <BackdropVideo autoPlay={true} muted={true} controls={false} loop={true} playsInline={true}>
-                    <source src="/video/people-on-kayak-intro.mp4" type="video/mp4" />
+                    {this.state.isBigScreen === true &&
+                        <source src="/video/people-on-kayak-intro.1920.mp4" type="video/mp4" media="all and (min-width: 1659px)" />
+                    }
+
+                    {this.state.isBigScreen === false &&
+                        <source src="/video/people-on-kayak-intro.1660.mp4" type="video/mp4" media="all and (max-width: 1660px)" />
+                    }
                 </BackdropVideo>
             </HomeWrapper>
-        )
+        );
     }
 }
